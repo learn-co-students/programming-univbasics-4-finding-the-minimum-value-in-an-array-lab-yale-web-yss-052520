1e4eb4ea072dfabe23a94bb15ef635c23faa4308
Ziming Mao <ziming.mao@yale.edu>
2020-05-02 23:59:40 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,13 @@
 def find_min_value(array)
   # Add your solution here
+  min = array[0]
+  i = 0
+  length = array.size
+  length.times do
+    if array[i] < min
+      min = array[i]
+    end
+    i += 1
+  end
+  min
 end
